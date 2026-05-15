5981b3481efabe3919fe2e854cf86e5db26c07bf
Hernando Enrique Moreno Moreno <hulkike@gmail.com>
2026-05-14 19:27:35 -0500

    chore: uno a muchos con DBRef

--- spring/project-yml/src/main/java/com/mycompany/project_yml/domain/Cliente.java
+++ spring/project-yml/src/main/java/com/mycompany/project_yml/domain/Cliente.java
@@ -56,6 +56,9 @@ public class Cliente implements Serializable {
     @Field("cuenta")
     private Cuenta cuenta;
 
+    @DBRef
+    @Field("facturas")
+    @JsonIgnoreProperties(value = { "cliente" }, allowSetters = true)
     private Set<Factura> facturaSet = new HashSet<>();
 
     public Cliente(String id, @Nonnull String numeroDocumento, @Nonnull String primerNombre, String segundoNombre, @Nonnull String primerApellido, String segundoApelligo) {
--- spring/project-yml/src/main/java/com/mycompany/project_yml/domain/Factura.java
+++ spring/project-yml/src/main/java/com/mycompany/project_yml/domain/Factura.java
@@ -1,8 +1,16 @@
 package com.mycompany.project_yml.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
 import java.util.Date;
 import java.util.Objects;
 
+@Document(collection = "factura")
+@CompoundIndex(name = "ixd_unique_factura", def = "{'numeroFactura': 1, 'anio': 1}", unique = true)
 public class Factura {
     private String id;
     private long numeroFactura;
@@ -12,6 +20,9 @@ public class Factura {
     private double iva;
     private double subtotal;
 
+    @DBRef
+    @Field("cliente")
+    @JsonIgnoreProperties(value = { "tipoDocumento", "cuenta", "facturas" }, allowSetters = true)
     private Cliente cliente;
 
     public Factura(String id, long numeroFactura, int anio, Date fecha, double total, double iva, double subtotal) {
