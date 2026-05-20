aef1090771e9abab5b4181fd52c9c669388897da
Hernando Enrique Moreno Moreno <hulkike@gmail.com>
2026-05-19 21:11:24 -0500

    chore: PUT /api/tipo-documentos/:id

--- spring/project-yml/src/main/java/com/mycompany/project_yml/web/rest/TipoDocumentoResource.java
+++ spring/project-yml/src/main/java/com/mycompany/project_yml/web/rest/TipoDocumentoResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -25,42 +26,56 @@ public class TipoDocumentoResource {
 
     // POST /api/document-types
     @PostMapping("/tipo-documentos") // endpoint to create a new document type
-    public ResponseEntity<TipoDocumento> createTipoDocumento(@RequestBody TipoDocumento documentType) throws URISyntaxException {
-        LOG.debug("se crea: {}", documentType);
-        if(documentType.getId() != null){
+    public ResponseEntity<TipoDocumento> createTipoDocumento(@RequestBody TipoDocumento tipoDocumento) throws URISyntaxException {
+        LOG.debug("se crea: {}", tipoDocumento);
+        if (tipoDocumento.getId() != null) {
             return ResponseEntity.badRequest().build();
-        }else {
-            TipoDocumento tipoDocumentoSaved = tipoDocumentoRepository.insert(documentType);
+        } else {
+            TipoDocumento tipoDocumentoSaved = tipoDocumentoRepository.insert(tipoDocumento);
             return ResponseEntity.created(new URI("api/tipo-documentos/" + tipoDocumentoSaved.getId())).body(tipoDocumentoSaved);
         }
     }
 
     // PUT /api/document-types
-    @PutMapping("/tipo-documentos")// endpoint to update an existing document type
-    public String updateTipoDocumento(@RequestBody TipoDocumento documentType) {
-        System.out.println("se actualiza: " + documentType.toString());
-        return "UPDATE DOCUMENT TYPE";
+    @PutMapping("/tipo-documentos/{id}")// endpoint to update an existing document type
+    public ResponseEntity<TipoDocumento> updateTipoDocumento(
+            @PathVariable(value = "id", required = false) final String id,
+            @RequestBody TipoDocumento tipoDocumento) {
+        LOG.debug("se actualiza: {}", tipoDocumento);
+        if (id == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        if (!tipoDocumento.getId().equals(id)) {
+            return ResponseEntity.badRequest().build();
+        }
+        Optional<TipoDocumento> tipoDocumentoUpdated = tipoDocumentoRepository.findById(id);
+        if (tipoDocumentoUpdated.isPresent()) {
+            TipoDocumento tipoDocumentoSaved = tipoDocumentoRepository.save(tipoDocumento);
+            return ResponseEntity.ok().body(tipoDocumentoSaved);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+
     }
+
     // GET /api/document-types
     @GetMapping("/tipo-documentos") // endpoint to get all document types
-    public ResponseEntity<List<TipoDocumento>> getTipoDocumento(){
+    public ResponseEntity<List<TipoDocumento>> getTipoDocumento() {
         List<TipoDocumento> tipoDocumentos = tipoDocumentoRepository.findAll();
         return ResponseEntity.ok().body(tipoDocumentos);
     }
 
     // GET /api/document-types/{id}
     @GetMapping("/tipo-documentos/{id}") // endpoint to get a document type by id
-    public String getTipoDocumentoById(@PathVariable("id") Long id){
+    public String getTipoDocumentoById(@PathVariable("id") Long id) {
         return "GET DOCUMENT TYPE BY ID: " + id;
     }
 
     // DELETE /api/document-types/{id}
     @DeleteMapping("/tipo-documentos/{id}") // endpoint to delete a document type by id
-    public String deleteTipoDocumento(@PathVariable("id") Long id){
+    public String deleteTipoDocumento(@PathVariable("id") Long id) {
         return "DELETE DOCUMENT TYPE BY ID: " + id;
     }
-
-
 
 
 }
