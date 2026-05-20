cf1830815646ac55a14e7cc7caa4994d19eeb169
Hernando Enrique Moreno Moreno <hulkike@gmail.com>
2026-05-19 19:33:04 -0500

    chore: se configura el loguer slf4j parar debuguear

--- spring/project-yml/src/main/java/com/mycompany/project_yml/domain/TipoDocumento.java
+++ spring/project-yml/src/main/java/com/mycompany/project_yml/domain/TipoDocumento.java
@@ -97,4 +97,14 @@ public class TipoDocumento implements Serializable {
     public int hashCode() {
         return Objects.hashCode(id);
     }
+
+    @Override
+    public String toString() {
+        return "TipoDocumento{" +
+                "id='" + id + '\'' +
+                ", sigla='" + sigla + '\'' +
+                ", nombreDocumento='" + nombreDocumento + '\'' +
+                ", estado=" + estado +
+                '}';
+    }
 }
