c0b7f30690179927cab98a53f04dd9d25a8511b1
Hernando Enrique Moreno Moreno <hulkike@gmail.com>
2026-05-20 19:43:04 -0500

    chore: GET /api/tipo-documentos/:id

--- spring/project-yml/src/main/java/com/mycompany/project_yml/repository/TipoDocumentoRepository.java
+++ spring/project-yml/src/main/java/com/mycompany/project_yml/repository/TipoDocumentoRepository.java
@@ -3,10 +3,18 @@ package com.mycompany.project_yml.repository;
 
 import com.mycompany.project_yml.domain.TipoDocumento;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.Optional;
 
 public interface TipoDocumentoRepository extends MongoRepository<TipoDocumento, String> {
     // method query
     Optional<TipoDocumento> findBySigla(String sigla);
+
+    @Query("""
+        {
+            'sigla': ?0
+        }
+    """)
+    Optional<TipoDocumento> buscarPorSigla(String sigla);
 }
